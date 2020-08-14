81796a92c593e62148df4ffd51564051dc6b3614
Roadagain <ringoh72@gmail.com>
2020-08-15 03:01:33 +0900

    不要な設定を削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,8 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   env: {
     node: true,
-  },
-  rules: {
-    'prettier/prettier': ['error', { usePretterrc: true }],
   },
 }
