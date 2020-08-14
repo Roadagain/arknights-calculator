dda7620488e05b7f00699f7f1ff4eb36a3e9f5af
Roadagain <ringoh72@gmail.com>
2020-08-15 01:13:34 +0900

    固定値バフと割合バフでテストの文言を似せる

--- tests/buff/total_buff_percentage.test.ts
+++ tests/buff/total_buff_percentage.test.ts
@@ -9,14 +9,14 @@ describe('割合バフのトータル倍率の計算', () => {
   })
 
   describe('割合バフが1つの場合', () => {
-    it('バフの倍率がトータル倍率になる', () => {
+    it('バフ倍率がトータル倍率になる', () => {
       const totalBuff = totalBuffPercentage([2])
       expect(totalBuff).toBe(2)
     })
   })
 
   describe('割合バフが複数ある場合', () => {
-    it('各バフの加算結果がトータル倍率になる', () => {
+    it('各バフ倍率の総和がトータル倍率になる', () => {
       const totalBuff = totalBuffPercentage([1.5, 2, 1.2])
       expect(totalBuff).toBe(2.7)
     })
