4ae5317b16253213bdf82da2b54c1b220661e2dd
Roadagain <ringoh72@gmail.com>
2020-08-15 02:40:52 +0900

    変な空白を消す

--- tests/attack/buffed_attack_value.test.ts
+++ tests/attack/buffed_attack_value.test.ts
@@ -44,7 +44,7 @@ describe('最終攻撃力の計算', () => {
 
   describe.each`
   buffNameA | buffNameB | buff | expected
-  ${ '攻撃力にnを加算'} | ${'攻撃力+n%'} | ${createAttackBuff({ fixedAddition: 200, percentageAddition: 1.5 })} | ${950}
+  ${'攻撃力にnを加算'} | ${'攻撃力+n%'} | ${createAttackBuff({ fixedAddition: 200, percentageAddition: 1.5 })} | ${950}
   `('加算されるバフの組み合わせ', ({ buffNameA, buffNameB, buff, expected }) => {
     it(`${buffNameA} と ${buffNameB} は加算される`, () => {
       const attack = buffedAttackValue(attackBase, buff)
@@ -54,11 +54,11 @@ describe('最終攻撃力の計算', () => {
 
   describe.each`
   buffNameA | buffNameB | buff | expected
-  ${ '攻撃力にnを加算'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ fixedAddition: 200, damageIncrease: 1.5 })} | ${1050}
-  ${ '攻撃力にnを加算'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ fixedAddition: 200, percentageIncrease: 1.5 })} | ${1050}
-  ${ '攻撃力+n%'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ percentageAddition: 1.5, damageIncrease: 1.5 })} | ${1125}
-  ${ '攻撃力+n%'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ percentageAddition: 1.5, percentageIncrease: 1.5 })} | ${1125}
-  ${ '攻撃力のn%のダメージ'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ damageIncrease: 1.5, percentageIncrease: 1.5 })} | ${1125}
+  ${'攻撃力にnを加算'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ fixedAddition: 200, damageIncrease: 1.5 })} | ${1050}
+  ${'攻撃力にnを加算'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ fixedAddition: 200, percentageIncrease: 1.5 })} | ${1050}
+  ${'攻撃力+n%'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ percentageAddition: 1.5, damageIncrease: 1.5 })} | ${1125}
+  ${'攻撃力+n%'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ percentageAddition: 1.5, percentageIncrease: 1.5 })} | ${1125}
+  ${'攻撃力のn%のダメージ'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ damageIncrease: 1.5, percentageIncrease: 1.5 })} | ${1125}
   `('乗算されるバフの組み合わせ', ({ buffNameA, buffNameB, buff, expected }) => {
     it(`${buffNameA} と ${buffNameB} は乗算される`, () => {
       const attack = buffedAttackValue(attackBase, buff)
