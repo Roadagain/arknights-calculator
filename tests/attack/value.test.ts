fc066c8bee1b2ba117596dcf67920305e36b44fb
Roadagain <ringoh72@gmail.com>
2020-08-15 00:54:14 +0900

    共通部分を関数やdescribe.eachでまとめる

--- tests/attack/value.test.ts
+++ tests/attack/value.test.ts
@@ -1,70 +1,70 @@
 import Attack from '../../src/attack'
+import AttackBuff, { IAttackBuff } from '../../src/attack-buff'
 
-describe('最終攻撃力の計算', () => {
-  describe('バフなしの場合', () => {
-    it('基礎値がそのまま攻撃力になる', () => {
-      const attack = new Attack(100)
-      expect(attack.value).toBe(100)
-    })
-  })
-
-  describe('攻撃力にnを加算', () => {
-    it('基礎値に加算される', () => {
-      const attack = new Attack(100, [10])
-      expect(attack.value).toBe(110)
-    })
-  })
-
-  describe('攻撃力+n%', () => {
-    it('基礎値に乗算される', () => {
-      const attack = new Attack(100, [], [1.1])
-      expect(attack.value).toBe(110)
-    })
-  })
-
-  describe('攻撃力のn%のダメージ', () => {
-    it('基礎値に乗算される', () => {
-      const attack = new Attack(100, [], [], [1.1])
-      expect(attack.value).toBe(110)
-    })
-  })
-
-  describe('攻撃力がn%まで上昇', () => {
-    it('基礎値に乗算される', () => {
-      const attack = new Attack(100, [], [], [1.1])
-      expect(attack.value).toBe(110)
-    })
-  })
-
-  const defaultBuffs = {
-    attackPlussesFixed: [],
+const attackBase = 100
+export const createAttackBuff = (buff: Partial<IAttackBuff> = {}): AttackBuff => {
+  const defaultBuff: IAttackBuff = {
     attackPlussesPercentage: [],
+    attackPlussesFixed: [],
     damageBuffs: [],
     attackBuffs: [],
   }
 
+  const { attackPlussesPercentage, attackPlussesFixed, damageBuffs, attackBuffs } = { ...defaultBuff, ...buff }
+  return new AttackBuff(attackPlussesPercentage, attackPlussesFixed, damageBuffs, attackBuffs)
+}
+
+describe('最終攻撃力の計算', () => {
+  describe('バフなしの場合', () => {
+    it('基礎値がそのまま攻撃力になる', () => {
+      const buff = createAttackBuff()
+      const attack = new Attack(attackBase, buff)
+      expect(attack.value).toBe(attackBase)
+    })
+  })
+
   describe.each`
-  buffNameA | buffNameB | buffs | expected
-  ${ '攻撃力にnを加算'} | ${'攻撃力+n%'} | ${{ ...defaultBuffs, attackPlussesFixed: [10], attackPlussesPercentage: [1.1] }} | ${120}
-  `('加算バフの組み合わせ', ({ buffNameA, buffNameB, buffs, expected }) => {
-    it(`${buffNameA} と ${buffNameB} は加算される`, () => {
-      const { attackPlussesFixed, attackPlussesPercentage, damageBuffs, attackBuffs } = buffs
-      const attack = new Attack(100, attackPlussesFixed, attackPlussesPercentage, damageBuffs, attackBuffs)
+  buffName | buff | expected
+  ${'攻撃力にnを加算'} | ${createAttackBuff({ attackPlussesFixed: [10] })} | ${110}
+  `('加算バフ', ({ buffName, buff, expected }) => {
+    it(`${buffName} は基礎値に加算される`, () => {
+      const attack = new Attack(attackBase, buff)
       expect(attack.value).toBe(expected)
     })
   })
 
   describe.each`
-  buffNameA | buffNameB | buffs | expected
-  ${ '攻撃力にnを加算'} | ${'攻撃力のn%のダメージ'} | ${{ ...defaultBuffs, attackPlussesFixed: [10], damageBuffs: [1.1] }} | ${121}
-  ${ '攻撃力にnを加算'} | ${'攻撃力がn%まで上昇'} | ${{ ...defaultBuffs, attackPlussesFixed: [10], attackBuffs: [1.1] }} | ${121}
-  ${ '攻撃力+n%'} | ${'攻撃力のn%のダメージ'} | ${{ ...defaultBuffs, attackPlussesPercentage: [1.1], damageBuffs: [1.1] }} | ${121}
-  ${ '攻撃力+n%'} | ${'攻撃力がn%まで上昇'} | ${{ ...defaultBuffs, attackPlussesPercentage: [1.1], attackBuffs: [1.1] }} | ${121}
-  ${ '攻撃力のn%のダメージ'} | ${'攻撃力がn%まで上昇'} | ${{ ...defaultBuffs, damageBuffs: [1.1], attackBuffs: [1.1] }} | ${121}
-  `('乗算バフの組み合わせ', ({ buffNameA, buffNameB, buffs, expected }) => {
+  buffName | buff | expected
+  ${'攻撃力+n%'} | ${createAttackBuff({ attackPlussesPercentage: [1.1] })} | ${110}
+  ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ damageBuffs: [1.1] })} | ${110}
+  ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ attackBuffs: [1.1] })} | ${110}
+  `('割合バフ', ({ buffName, buff, expected }) => {
+    it(`${buffName} は基礎値に割合加算される`, () => {
+      const attack = new Attack(attackBase, buff)
+      expect(attack.value).toBe(expected)
+    })
+  })
+
+  describe.each`
+  buffNameA | buffNameB | buff | expected
+  ${ '攻撃力にnを加算'} | ${'攻撃力+n%'} | ${createAttackBuff({ attackPlussesFixed: [10], attackPlussesPercentage: [1.1] })} | ${120}
+  `('加算されるバフの組み合わせ', ({ buffNameA, buffNameB, buff, expected }) => {
+    it(`${buffNameA} と ${buffNameB} は加算される`, () => {
+      const attack = new Attack(attackBase, buff)
+      expect(attack.value).toBe(expected)
+    })
+  })
+
+  describe.each`
+  buffNameA | buffNameB | buff | expected
+  ${ '攻撃力にnを加算'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ attackPlussesFixed: [10], damageBuffs: [1.1] })} | ${121}
+  ${ '攻撃力にnを加算'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ attackPlussesFixed: [10], attackBuffs: [1.1] })} | ${121}
+  ${ '攻撃力+n%'} | ${'攻撃力のn%のダメージ'} | ${createAttackBuff({ attackPlussesPercentage: [1.1], damageBuffs: [1.1] })} | ${121}
+  ${ '攻撃力+n%'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ attackPlussesPercentage: [1.1], attackBuffs: [1.1] })} | ${121}
+  ${ '攻撃力のn%のダメージ'} | ${'攻撃力がn%まで上昇'} | ${createAttackBuff({ damageBuffs: [1.1], attackBuffs: [1.1] })} | ${121}
+  `('乗算されるバフの組み合わせ', ({ buffNameA, buffNameB, buff, expected }) => {
     it(`${buffNameA} と ${buffNameB} は乗算される`, () => {
-      const { attackPlussesFixed, attackPlussesPercentage, damageBuffs, attackBuffs } = buffs
-      const attack = new Attack(100, attackPlussesFixed, attackPlussesPercentage, damageBuffs, attackBuffs)
+      const attack = new Attack(attackBase, buff)
       expect(attack.value).toBe(expected)
     })
   })
