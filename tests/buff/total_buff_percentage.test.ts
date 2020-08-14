395e6fbd4a2f035fadc6a08db3e167876d506e7b
Roadagain <ringoh72@gmail.com>
2020-08-15 01:08:11 +0900

    テストに使う文言をより具体的にする

--- tests/buff/total_buff_percentage.test.ts
+++ tests/buff/total_buff_percentage.test.ts
@@ -1,7 +1,7 @@
 import { totalBuffPercentage } from '../../src/buff'
 
-describe('割合バフのトータル値の計算', () => {
-  describe('割合バフがない場合は1倍', () => {
+describe('割合バフのトータル倍率の計算', () => {
+  describe('割合バフがない場合', () => {
     it('1倍になる', () => {
       const totalBuff = totalBuffPercentage([])
       expect(totalBuff).toBe(1)
@@ -9,14 +9,14 @@ describe('割合バフのトータル値の計算', () => {
   })
 
   describe('割合バフが1つの場合', () => {
-    it('バフの値がトータル値になる', () => {
+    it('バフの倍率がトータル倍率になる', () => {
       const totalBuff = totalBuffPercentage([2])
       expect(totalBuff).toBe(2)
     })
   })
 
   describe('割合バフが複数ある場合', () => {
-    it('各バフの加算結果がトータル値になる', () => {
+    it('各バフの加算結果がトータル倍率になる', () => {
       const totalBuff = totalBuffPercentage([1.5, 2, 1.2])
       expect(totalBuff).toBe(2.7)
     })
