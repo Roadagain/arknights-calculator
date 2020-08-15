856002927959ee616ceaf6d9baf9c290cb164a46
Roadagain <ringoh72@gmail.com>
2020-08-15 18:28:47 +0900

    術耐性デバフの計算を実装

--- src/arts-defence.ts
+++ src/arts-defence.ts
@@ -6,3 +6,12 @@ export type ArtsDefenceBuff = {
 export const buffedArtsDefence = (base: number, buff: ArtsDefenceBuff): number => {
   return (base + buff.fixedAddition) * buff.percentageAddition
 }
+
+export type ArtsDefenceDebuff = {
+  readonly fixedSubtraction: number
+  readonly percentageSubtraction: number
+}
+
+export const debuffedArtsDefence = (base: number, debuff: ArtsDefenceDebuff): number => {
+  return (base - debuff.fixedSubtraction) * (1 - debuff.percentageSubtraction)
+}
