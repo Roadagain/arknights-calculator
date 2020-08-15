dd50c43f86c38469092c93381b51e42bcbe3836e
Roadagain <ringoh72@gmail.com>
2020-08-16 00:52:20 +0900

    術ダメージの計算を実装

--- src/damage.ts
+++ src/damage.ts
@@ -10,3 +10,8 @@ export const physicalDamage = (attack: number, defence: number, buff: DamageBuff
   const damage = Math.round((attack - defence) * buff.percentageIncrease)
   return Math.max(minimumDamage(attack), damage)
 }
+
+export const artsDamage = (attack: number, artsDefence: number, buff: DamageBuff): number => {
+  const damage = Math.round(attack * (1 - artsDefence / 100) * buff.percentageIncrease)
+  return Math.max(minimumDamage(attack), damage)
+}
---
+++ tests/damage/arts_damage.test.ts
@@ -0,0 +1,79 @@
+import { artsDamage, DamageBuff, minimumDamage } from '../../src/damage'
+
+describe('術ダメージ計算', () => {
+  const createDamageBuff = (buff: Partial<DamageBuff>): DamageBuff => {
+    return {
+      percentageIncrease: 1,
+      ...buff,
+    }
+  }
+
+  describe('計算結果が最低ダメージ以上になる場合', () => {
+    const attack = 550
+    const artsDefence = 40
+
+    describe('バフなしだった場合', () => {
+      const buff = createDamageBuff({})
+
+      it('攻撃力*(1-術耐性/100) がダメージになる', () => {
+        const expected = attack * (1 - artsDefence / 100)
+        const damage = artsDamage(attack, artsDefence, buff)
+        expect(damage).toBe(expected)
+      })
+
+      it('小数点以下は四捨五入される', () => {
+        const attackFloored = 502
+        const expectedFloored = Math.floor(attackFloored * (1 - artsDefence / 100))
+        const damageFloored = artsDamage(attackFloored, artsDefence, buff)
+        expect(damageFloored).toBe(expectedFloored)
+
+        const attackCeiled = 503
+        const expectedCeiled = Math.ceil(attackCeiled * (1 - artsDefence / 100))
+        const damageCeiled = artsDamage(attackCeiled, artsDefence, buff)
+        expect(damageCeiled).toBe(expectedCeiled)
+      })
+    })
+
+    describe('バフありだった場合', () => {
+      it('攻撃力*(1-術耐性/100)*ダメージバフ がダメージになる', () => {
+        const percentageIncrease = 1.3
+        const expected = attack * (1 - artsDefence / 100) * percentageIncrease
+        const buff = createDamageBuff({ percentageIncrease })
+        const damage = artsDamage(attack, artsDefence, buff)
+        expect(damage).toBe(expected)
+      })
+
+      it('小数点以下は四捨五入される', () => {
+        const percentageIncreaseFloored = 1.04
+        const expectedFloored = Math.floor(attack * (1 - artsDefence / 100) * percentageIncreaseFloored)
+        const buffFloored = createDamageBuff({ percentageIncrease: percentageIncreaseFloored })
+        const damageFloored = artsDamage(attack, artsDefence, buffFloored)
+        expect(damageFloored).toBe(expectedFloored)
+
+        const percentageIncreaseCeiled = 1.05
+        const expectedCeiled = Math.ceil(attack * (1 - artsDefence / 100) * percentageIncreaseCeiled)
+        const buffCeiled = createDamageBuff({ percentageIncrease: percentageIncreaseCeiled })
+        const damageCeiled = artsDamage(attack, artsDefence, buffCeiled)
+        expect(damageCeiled).toBe(expectedCeiled)
+      })
+    })
+  })
+
+  describe.each`
+    testcase      | partialBuff
+    ${'バフなし'} | ${{}}
+    ${'バフあり'} | ${{ percentageIncrease: 1.2 }}
+  `('計算結果が最低ダメージを下回る場合', ({ testcase, partialBuff }) => {
+    const attack = 500
+    const artsDefence = 100
+    const buff = createDamageBuff(partialBuff)
+
+    describe(`${testcase}だった場合`, () => {
+      it('最低ダメージになる', () => {
+        const expected = minimumDamage(attack)
+        const damage = artsDamage(attack, artsDefence, buff)
+        expect(damage).toBe(expected)
+      })
+    })
+  })
+})
